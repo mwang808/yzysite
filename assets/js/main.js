ec20cab126cbe689f55279f9a6080c1b32101ad7
Min Wang <mwang808@gmail.com>
2026-04-15 08:30:54 -0400

    Make site mobile friendly

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-console.log("Static site loaded");
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.querySelector(".menu-toggle");
+  const nav = document.querySelector(".nav");
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("active");
+    });
+  }
+});
+``
